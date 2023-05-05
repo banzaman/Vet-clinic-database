74ed3b4b28ae591fc938385572e7ba3d408a1787
Mark Rubanza <markrubanza10@gmail.com>
2023-05-05 04:58:50 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -3,5 +3,3 @@
 CREATE TABLE animals (
     name varchar(100)
 );
-postgres=# CREATE DATABASE Vet_clinic;
-CREATE TABLE animals(id int, name char(255), date_of_birth date, escape_attempts int, neutered boolean, weight_kg decimal(10, 2));
